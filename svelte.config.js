88d51b0673134283360cbd173f98bdff348ef077
Dana Woodman <dana@danawoodman.com>
2021-04-11 12:46:06 -0700

    Add API endpoint, config svelte for netlify

---
+++ svelte.config.js
@@ -0,0 +1,25 @@
+import sveltePreprocess from 'svelte-preprocess'
+import netlify from '@sveltejs/adapter-netlify'
+import pkg from './package.json'
+
+/** @type {import('@sveltejs/kit').Config} */
+module.exports = {
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: sveltePreprocess(),
+  kit: {
+    // By default, `npm run build` will create a standard Node app.
+    // You can create optimized builds for different platforms by
+    // specifying a different adapter
+    adapter: netlify(),
+
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: '#svelte',
+
+    vite: {
+      ssr: {
+        noExternal: Object.keys(pkg.dependencies || {}),
+      },
+    },
+  },
+}
